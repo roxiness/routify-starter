c8f8ccc08d7fe22d63cfca45ac1d699097282913
Jakob Rosenberg <jakobrosenberg@gmail.com>
2019-12-18 19:42:01 +0100

    added routify links

--- src/pages/example/index.svelte
+++ src/pages/example/index.svelte
@@ -1,3 +1,18 @@
 <div style="width: 100%; text-align: center; margin-top: 4rem;">
   <img src="/example/Routify-t.png" alt="" />
+  <div>
+    <b>Guide:</b>
+    <br />
+    <a href="https://routify.now.sh">https://routify.now.sh</a>
+  </div>
+  
+    <br />
+  <div>
+    <b>Template:</b>
+    <br />
+    <a href="https://github.com/sveltech/routify-starter">
+      https://github.com/sveltech/routify-starter
+    </a>
+  </div>
+
 </div>
