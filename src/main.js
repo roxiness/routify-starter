c7c013d675786b4ebcb874a39eca35ad014b9c2a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-01-19 21:01:02 +0100

    prep main.js for HMR

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
+import HMR from  '@sveltech/routify/hmr'
 import App from './App.svelte';
 
-const app = new App({
-	target: document.body
-});
+HMR(App, { target: document.body }, 'routify-app')
 
 export default app;
