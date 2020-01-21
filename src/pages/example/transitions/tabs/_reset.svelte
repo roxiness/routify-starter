67de15044218aec4ebf6e2c0b315c09147417e0f
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-01-21 17:03:00 +0100

    Add urlOrder to transitions (may not be needed)

--- src/pages/example/transitions/tabs/_reset.svelte
+++ src/pages/example/transitions/tabs/_reset.svelte
@@ -4,21 +4,22 @@
   import BottomNav from "./_components/BottomNav.svelte";
   import { url, isActive } from "@sveltech/routify";
 
-
   const width = writable();
   const color = writable();
   const _urls = [
-    ["Home", "./home", "#7fc5bb"],
-    ["Feed", "./feed", "#0bf5cc"],
-    ["Updates", "./updates", "#88f0d0"],
-    ["Settings", "./settings", "#a1fac3"]
+    ["./home", "Home", "#7fc5bb"],
+    ["./feed", "Feed", "#0bf5cc"],
+    ["./updates", "Updates", "#88f0d0"],
+    ["./settings", "Settings", "#a1fac3"]
   ];
-  $: urls = _urls.map(([name, path, color]) => ({
+  $: urls = _urls.map(([path, name, color]) => ({
     name,
     href: $url(path),
     color,
     active: !!$isActive(path)
   }));
+  
+  $: urlOrder = urls.map(({href}) => href);
 </script>
 
 <style>
@@ -53,7 +54,7 @@
 <div style="height: 100%">
 
   <main class="inset" bind:offsetWidth={$width}>
-    <slot decorator={TabsTransition} scoped={{ width }} />
+    <slot decorator={TabsTransition} scoped={{ width, urls: urlOrder }} />
   </main>
   <BottomNav {urls} height="64px" />
 </div>
