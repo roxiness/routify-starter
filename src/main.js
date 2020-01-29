6611f85111465cf0d17b480c13a98c211de3660d
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-01-29 21:41:52 +0100

    fix app export

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import HMR from  '@sveltech/routify/hmr'
 import App from './App.svelte';
 
-HMR(App, { target: document.body }, 'routify-app')
+const app = HMR(App, { target: document.body }, 'routify-app')
 
 export default app;
