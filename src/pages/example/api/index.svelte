1624ef3e24c679872e6476b49612a720c8ad92e8
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-01-30 21:15:35 +0100

    rewrite api example

--- src/pages/example/api/index.svelte
+++ src/pages/example/api/index.svelte
@@ -1,28 +1,14 @@
 <script>
   import { url } from "@sveltech/routify";
-  import { fakeapi } from "../_fakeapi";
+  export let scoped;
+  const { movies } = scoped;
+  console.log(movies);
 </script>
 
-<style>
-  .card {
-    display: block;
-    cursor: pointer;
-  }
-</style>
-
-<div style="width: 512px; margin: auto; overflow: auto">
-  {#await fakeapi()}
-    Loading page...
-  {:then songs}
-    <ul>
-      {#each songs as { title, slug }}
-        <a
-          href={$url('../:slug', { slug })}
-          class="card"
-          style="text-align:center">
-          {title}
-        </a>
-      {/each}
-    </ul>
-  {/await}
+<div style="text-align: center">
+  {#each movies as [showId, title]}
+    <h3>
+      <a href={$url('../:showId', { showId })}>{title}</a>
+    </h3>
+  {/each}
 </div>
