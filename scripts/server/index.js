01ab58cb933c92b4f0a30995f3ed33028662e54e
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-02-20 11:18:46 +0100

    Add a little transparency to the server script

--- scripts/server/index.js
+++ scripts/server/index.js
@@ -7,11 +7,14 @@ const distDir = '../../dist'
 const bundleDir = `${distDir}/build/bundle.js`
 const templateDir = `${distDir}/__app.html`
 
+// Serve assets and prerendered pages
 app.use(express.static(distDir))
 
+// Fallback to SSR rendering if the file doesn't exist
 app.get('*', async (req, res) => {
     const HTML = await ssr(templateDir, bundleDir, req.url)
     res.send(HTML)
 })
 
+console.log('Serving on http://localhost:' + port)
 app.listen(port)
