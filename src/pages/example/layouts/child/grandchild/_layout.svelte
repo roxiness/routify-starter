ea1a2f69fcbefbb0381f9b2e8757b53ad3672e1b
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-14 16:55:00 +0200

    update layout example layouts

--- src/pages/example/layouts/child/grandchild/_layout.svelte
+++ src/pages/example/layouts/child/grandchild/_layout.svelte
@@ -2,19 +2,9 @@
   import { url } from "@sveltech/routify";
 </script>
 
-<style>
-  .card {
-    width: 66%;
-    margin: 16px;
-    padding: 16px;
-    border-radius: 4px;
-    background: white;
-    box-shadow: 0px 5px 20px 5px rgba(0, 0, 0, 0.075);
-    display: inline-block;
-  }
-</style>
 
-<div style="text-align: center;">
+
+<div class="layout-container">
   <div class="card">
     <slot>
       <!-- optional fallback -->
