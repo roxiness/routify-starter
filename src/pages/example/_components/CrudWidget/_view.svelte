1cc96d4c0c61da21fa3cb5fd185249016cc94daf
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-05-06 17:04:39 +0200

    fix error when navigating away from widget

--- src/pages/example/_components/CrudWidget/_view.svelte
+++ src/pages/example/_components/CrudWidget/_view.svelte
@@ -5,16 +5,18 @@
   $: item = data.filter(item => item.id == id)[0];
 </script>
 
-<div>
+{#if item}
   <div>
-    {#each Object.entries(item) as [name, value]}
-      <div>
-        <b>{name}:</b>
-        {value}
-      </div>
-    {/each}
+    <div>
+      {#each Object.entries(item) as [name, value]}
+        <div>
+          <b>{name}:</b>
+          {value}
+        </div>
+      {/each}
+    </div>
+    <br />
+    <a href={$url('../', { id })}>[Back]</a>
+    <a href={$url('../:id/update', { id })}>[Update]</a>
   </div>
-  <br />
-  <a href={$url('../', { id })}>[Back]</a>
-  <a href={$url('../:id/update', { id })}>[Update]</a>
-</div>
+{/if}
