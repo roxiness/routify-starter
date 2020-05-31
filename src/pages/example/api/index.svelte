b7bb891ae1b336ae7d8459e266311d362e38b7bf
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-05-31 21:58:49 +0200

    add prefetching to api page

--- src/pages/example/api/index.svelte
+++ src/pages/example/api/index.svelte
@@ -1,13 +1,20 @@
 <script>
-  import { url } from "@sveltech/routify";
+  import { url, prefetch } from "@sveltech/routify";
   export let scoped;
+  const options = {
+    validFor: 3600 * 24 * 31, // don't refresh assets on the page for a month
+    writeHeaders: true // useful for debugging
+  };
   const { movies } = scoped;
+  // console.log(movies)
 </script>
 
 <div style="text-align: center">
   {#each movies as [showId, title]}
     <h3>
-      <a href={$url('../:showId', { showId })}>{title}</a>
+      <a use:prefetch={options} href={$url('../:showId', { showId })}>
+        {title}
+      </a>
     </h3>
   {/each}
 </div>
