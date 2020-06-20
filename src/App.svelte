10846f39af1d67118087f480b3b476f24915757a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-20 17:36:41 +0200

    routes destination

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Router } from "@sveltech/routify";
-  import { routes } from "../.routify/routes";
+  import { routes } from "@sveltech/routify/tmp/routes";
 </script>
 
 <Router {routes} />
