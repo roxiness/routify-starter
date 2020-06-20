3f57c18876cb29e89e1d56438f5fac15484b1a4a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-20 17:36:52 +0200

    disable service worker by default

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ const app = HMR(App, { target: document.body }, 'routify-app')
 export default app;
 
 
-if ('serviceWorker' in navigator) {
-    import('workbox-window').then(async ({ Workbox }) => {
-        const wb = new Workbox('/sw.js')
-        const registration = await wb.register()
-        wb.addEventListener('installed', () => (console.log('installed service worker')))
-        wb.addEventListener('externalinstalled', () => (console.log('installed service worker')))  
-    })
-}
+/** Service worker. Uncomment to use service worker */
+
+// if ('serviceWorker' in navigator) {
+//     import('workbox-window').then(async ({ Workbox }) => {
+//         const wb = new Workbox('/sw.js')
+//         const registration = await wb.register()
+//         wb.addEventListener('installed', () => (console.log('installed service worker')))
+//         wb.addEventListener('externalinstalled', () => (console.log('installed service worker')))  
+//     })
+// }
