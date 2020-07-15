b646f29d937dce2a455cf95bca34ee03e5e7d447
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-07-15 21:09:47 +0200

    meta fix

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,6 +1,10 @@
 <script>
+  import { metatags, page } from "@sveltech/routify";
   import Navigation from "./_navigation.svelte";
   import SrcOnGithub from "../components/SrcOnGithub.svelte";
+
+  $: metatags.title = `My Routify app - ${$page.title}`;
+  metatags.description = "Description coming soon...";
 </script>
 
 <div class="container">
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,9 +1,3 @@
-<script>
-  import { metatags } from "@sveltech/routify";
-  metatags.title = "My Routify app";
-  metatags.description = "Description coming soon...";
-</script>
-
 <div class="hero">
   <h1>Routify blog example</h1>
   <code>npx @sveltech/routify init --branch blog</code>
