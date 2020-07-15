fb9af825e21c7c1a9fa9a2f1dd94c6492856f869
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-07-15 21:38:15 +0200

    checkboxes

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,6 +1,14 @@
 <div class="hero">
   <h1>Routify blog example</h1>
   <code>npx @sveltech/routify init --branch blog</code>
+  
+  <ul class="checks">
+    <li>☑ Markdown with mdsvex</li>
+    <li>☑ PostCSS</li>
+    <li>☑ Auto generated navigation</li>
+    <li>☑ Auto generated page titles</li>
+    <li>☑ 100% Lighthouse score</li>
+  </ul>
 </div>
 
 <!-- routify:options index=10 -->
