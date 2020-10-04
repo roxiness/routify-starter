c03fb68296cc0679a969c5708befa44dab648534
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-04 20:50:10 +0200

    fix ssr resolve path

--- api/vercel-ssr.js
+++ api/vercel-ssr.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const { tossr } = require('tossr')
 
-const script = fs.readFileSync(require.resolve('../public/build/bundle.js'), 'utf8')
-const template = fs.readFileSync(require.resolve('../public/__app.html'), 'utf8')
+const script = fs.readFileSync(require.resolve('../dist/build/main.js'), 'utf8')
+const template = fs.readFileSync(require.resolve('../dist/__app.html'), 'utf8')
 
 module.exports = async (req, res) => {
     const html = await tossr(template, script, req.url)
