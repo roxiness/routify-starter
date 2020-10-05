f9896c2af1fa52c9e9ab6b7b48b94e5a7ac36605
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-05 17:00:45 +0200

    logit

--- api/vercel-ssr.js
+++ api/vercel-ssr.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const { tossr } = require('tossr')
 
-const script = fs.readFileSync(require.resolve('../build/main.js'), 'utf8')
-const template = fs.readFileSync(require.resolve('../__app.html'), 'utf8')
+// const script = fs.readFileSync(require.resolve('../build/main.js'), 'utf8')
+// const template = fs.readFileSync(require.resolve('../__app.html'), 'utf8')
 
 module.exports = async (req, res) => {
-    const html = await tossr(template, script, req.url)
-    res.send(html + '\n<!--ssr rendered-->')
+    console.log('same', fs.readdirSync('.'))
+    console.log('sparent', fs.readdirSync('..'))
+    res.send('hello')
+    // const html = await tossr(template, script, req.url)
+    // res.send(html + '\n<!--ssr rendered-->')
 }
 
