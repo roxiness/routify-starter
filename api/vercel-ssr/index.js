90e37ae478f8f921d16915af11cb5a75fd4e3731
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-25 16:58:17 +0100

    resolve path

--- api/vercel-ssr/index.js
+++ api/vercel-ssr/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
+const { resolve } = require('path')
 const { tossr } = require('tossr')
 
 // const script = fs.readFileSync(require.resolve('../build/main.js'), 'utf8')
 // const template = fs.readFileSync(require.resolve('../__app.html'), 'utf8')
-const template = '../../__app.html'
-const script = '../../build/main.js'
+const template = resolve('..', '..', '__app.html')
+const script = resolve('..', '../build/main.js')
 
 module.exports = async (req, res) => {
 
