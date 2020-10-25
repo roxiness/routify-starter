78fc0f28c8f68a1df676239f0b3a4ba2f499ca28
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-25 16:38:44 +0100

    build function in CI

--- api/vercel-ssr/build.js
+++ api/vercel-ssr/build.js
@@ -1,19 +1,36 @@
-const fs = require('fs')
+const { resolve } = require('path')
+const {existsSync, readdirSync} = require('fs')
+const { execFileSync } = require('child_process')
 
-const build = async () => {
+
+const shouldBuild = process.env.NOW_GITHUB_DEPLOYMENT
+
+
+if (shouldBuild)
+    build()
+else
+    waitForAppToExist()
+
+
+
+function build() {
+    execFileSync('yarn run now-build', { cwd: resolve('..', '..') })
+}
+
+
+async function waitForAppToExist() {
     let appExists = false
     console.log(process.env)
-    // while (!appExists) {
-    //     console.log({
+    while (!appExists) {
+        console.log({
 
-    //         'same': fs.readdirSync(__dirname),
-    //         'parent': fs.readdirSync(__dirname + '/..'),
-    //         'grandparent': fs.readdirSync(__dirname + '/../..')
-    //     })
-    //     console.log(`checking if ../../dist/build/main.js exists`)
-    //     appExists = fs.existsSync('../../dist/build/main.js')
-    //     await new Promise(r => setTimeout(r, 2000))
-    // }
-    // console.log(`../../dist/build/main.js exists exists`)
+            'same': readdirSync(__dirname),
+            'parent': readdirSync(__dirname + '/..'),
+            'grandparent': readdirSync(__dirname + '/../..')
+        })
+        console.log(`checking if ../../dist/build/main.js exists`)
+        appExists = existsSync('../../dist/build/main.js')
+        await new Promise(r => setTimeout(r, 2000))
+    }
+    console.log(`../../dist/build/main.js exists exists`)
 }
-build()
