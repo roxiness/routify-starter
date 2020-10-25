4df3d8fa880bf317b1129e15ef4ddac32a51cf7a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-25 16:54:15 +0100

    use tossr

--- api/vercel-ssr/index.js
+++ api/vercel-ssr/index.js
@@ -3,15 +3,17 @@ const { tossr } = require('tossr')
 
 // const script = fs.readFileSync(require.resolve('../build/main.js'), 'utf8')
 // const template = fs.readFileSync(require.resolve('../__app.html'), 'utf8')
+const template = '../../__app.html'
+const script = '../../build/main.js'
 
 module.exports = async (req, res) => {
 
-    res.send(JSON.stringify({
-        'same': fs.readdirSync(__dirname),
-        'parent': fs.readdirSync(__dirname+'/..'),
-        'grandparent': fs.readdirSync(__dirname+'/../..')
-    }, null, 2))
-    // const html = await tossr(template, script, req.url)
-    // res.send(html + '\n<!--ssr rendered-->')
+    // res.send(JSON.stringify({
+    //     'same': fs.readdirSync(__dirname),
+    //     'parent': fs.readdirSync(__dirname+'/..'),
+    //     'grandparent': fs.readdirSync(__dirname+'/../..')
+    // }, null, 2))
+    const html = await tossr(template, script, req.url)
+    res.send(html + '\n<!--ssr rendered-->')
 }
 
