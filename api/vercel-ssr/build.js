9bd82748a7b4e8d6ca8e6e490ba3d291774baa87
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-25 16:18:57 +0100

    log env

--- api/vercel-ssr/build.js
+++ api/vercel-ssr/build.js
@@ -2,17 +2,18 @@ const fs = require('fs')
 
 const build = async () => {
     let appExists = false
-    while (!appExists) {
-        console.log({
+    console.log(process.env)
+    // while (!appExists) {
+    //     console.log({
 
-            'same': fs.readdirSync(__dirname),
-            'parent': fs.readdirSync(__dirname + '/..'),
-            'grandparent': fs.readdirSync(__dirname + '/../..')
-        })
-        console.log(`checking if ../../dist/build/main.js exists`)
-        appExists = fs.existsSync('../../dist/build/main.js')
-        await new Promise(r => setTimeout(r, 2000))
-    }
-    console.log(`../../dist/build/main.js exists exists`)
+    //         'same': fs.readdirSync(__dirname),
+    //         'parent': fs.readdirSync(__dirname + '/..'),
+    //         'grandparent': fs.readdirSync(__dirname + '/../..')
+    //     })
+    //     console.log(`checking if ../../dist/build/main.js exists`)
+    //     appExists = fs.existsSync('../../dist/build/main.js')
+    //     await new Promise(r => setTimeout(r, 2000))
+    // }
+    // console.log(`../../dist/build/main.js exists exists`)
 }
 build()
