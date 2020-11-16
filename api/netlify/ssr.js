e3fcd86f1a58e65c0905d1ba8012c957efd7022a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-11-16 09:33:10 +0100

    stab at netlify

---
+++ api/netlify/ssr.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const { tossr } = require('tossr')
+
+const script =  'dist/build/main.js'
+const template = 'assets/__app.html'
+
+exports.handler = async (event, context) => {
+    const body = await tossr(template, script, event.path, { inlineDynamicImports: true })
+    return { statusCode: 200, body: body + '\n<!--ssr rendered-->' }
+}
