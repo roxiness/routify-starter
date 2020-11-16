2fb40d034688c89ef7604ffe960e32e13bc90479
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-11-16 09:56:45 +0100

    resolve script and template in ssr.js

--- api/netlify/ssr.js
+++ api/netlify/ssr.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const { tossr } = require('tossr')
 
-const script =  'dist/build/main.js'
-const template = 'assets/__app.html'
+const script = fs.readFileSync(require.resolve('../../dist/build/main.js'), 'utf8')
+const template = fs.readFileSync(require.resolve('../../assets/__app.html'), 'utf8')
 
 exports.handler = async (event, context) => {
-    const body = await tossr(template, script, event.path, { inlineDynamicImports: true })
+    const body = await tossr(template, script, event.path, {inlineDynamicImports: true})
     return { statusCode: 200, body: body + '\n<!--ssr rendered-->' }
 }
