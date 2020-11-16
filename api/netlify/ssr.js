337166156677de5c61ccaeb1f9534768da976356
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-11-16 10:01:33 +0100

    copy script & template to netlify

--- api/netlify/ssr.js
+++ api/netlify/ssr.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const { tossr } = require('tossr')
 
-const script = fs.readFileSync(require.resolve('../../dist/build/main.js'), 'utf8')
-const template = fs.readFileSync(require.resolve('../../assets/__app.html'), 'utf8')
+const script = 'main.js'
+const template = '__app.html'
 
 exports.handler = async (event, context) => {
-    const body = await tossr(template, script, event.path, {inlineDynamicImports: true})
+    const body = await tossr(template, script, event.path, { inlineDynamicImports: true })
     return { statusCode: 200, body: body + '\n<!--ssr rendered-->' }
 }
