a8879398d98ae803a7afb1500651089cb4560b79
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-11-16 10:17:43 +0100

    do some logging

--- api/netlify/ssr.js
+++ api/netlify/ssr.js
@@ -4,6 +4,9 @@ const { tossr } = require('tossr')
 const script = 'dist/build/main.js'
 const template = 'dist/__app.html'
 
+console.log('dir')
+console.log(fs.readdirSync(__dirname))
+
 exports.handler = async (event, context) => {
     const body = await tossr(template, script, event.path, { inlineDynamicImports: true })
     return { statusCode: 200, body: body + '\n<!--ssr rendered-->' }
