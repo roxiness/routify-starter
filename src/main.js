908549e5bfa297cbe8d755ed2045e7aaeda75696
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-11-20 14:54:00 +0100

    serviceworker

--- src/main.js
+++ src/main.js
@@ -4,19 +4,3 @@ import App from './App.svelte';
 const app = HMR(App, { target: document.body }, 'routify-app')
 
 export default app;
-
-
-/** Service worker. Uncomment to use service worker */
-
-// if ('serviceWorker' in navigator) {
-//     addEventListener('app-loaded', () => {
-//         window.requestIdleCallback(() =>
-//             import('workbox-window').then(async ({ Workbox }) => {
-//                 const wb = new Workbox('/serviceworker.js')
-//                 const registration = await wb.register()
-//                 wb.addEventListener('installed', () => (console.log('installed service worker')))
-//                 wb.addEventListener('externalinstalled', () => (console.log('installed service worker')))
-//             })
-//         )
-//     })
-// }
