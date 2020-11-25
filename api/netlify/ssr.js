e90d88315f7fa05dc8d1c9a45945634c689e9a08
Ignacio Bustos <ignacio.bustos@netcentric.biz>
2020-11-25 16:48:07 +0100

    Fix queryparams in netlify

--- api/netlify/ssr.js
+++ api/netlify/ssr.js
@@ -3,6 +3,9 @@ const { tossr } = require('tossr')
 const { script, template } = require('./bundle.json')
 
 exports.handler = async (event, context) => {
-    const body = await tossr(template, script, event.path)
+    const qs = Object.entries(event.queryStringParameters)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('&');
+    const body = await tossr(template, script, `${event.path}?${qs}`);
     return { statusCode: 200, body: body + '\n<!--ssr rendered-->' }
 }
