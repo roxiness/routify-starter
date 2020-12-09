5db5d385ae7e1b6fb33ee9799b5e7b3973a3de69
GHOST <ghostdevbusiness@gmail.com>
2020-12-09 16:36:13 +0000

    fix: update routes location

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { Router } from '@roxi/routify'
-	import { routes } from '@roxi/routify/tmp/routes'
+	import { routes } from '../.routify/routes'
 	import { authStore } from './auth.js'
 
 	// we need to queue our init till after Routify has been initialized
