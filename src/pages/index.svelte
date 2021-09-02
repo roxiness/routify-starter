b1b9ba7a919a331774a8e69df6c2465101d655e6
GHOST <ghostdevbusiness@gmail.com>
2021-09-02 22:35:22 +0100

    chore: update routify starter

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,9 +1,16 @@
 <script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
     import { metatags } from '@roxi/routify'
     
     metatags.title = 'My Routify app'
     metatags.description = 'Description coming soon...'
 </script>
 
-<RoutifyIntro />
+<h1>Routify Starter Template</h1>
+
+<p>
+    To see an example app that shows off a lot of Routify's features, go to <a href="/example">/example</a>
+</p>
+
+<p>
+    This template is ready to be used in production! just delete the example app at: src/pages/example
+</p>
