4a2e44979ebc36851eef7e7f110f388c17488fa0
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-01-09 19:45:19 +0100

    create spankable

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import viteMainJs from "vite-main-js";
 
 export default defineConfig({
-    server: {
-        port: 5000,
-    },
-    
-    plugins: [svelte()],
+  server: {
+    port: 5000,
+  },
+  build: { polyfillModulePreload: false },
+  plugins: [svelte(), viteMainJs()],
 });
