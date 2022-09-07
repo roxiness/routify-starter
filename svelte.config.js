af094e090d392cf6d5b12c8a3b99c731867fd06e
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-09-07 09:13:02 +0200

    fix: vite/svelte config

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-const preprocess = require('svelte-preprocess');
-
-module.exports = {
-    disableDependencyReinclusion: ['@roxi/routify'],
-
-    preprocess: [
-        preprocess(),
-    ],
-};
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preprocess from "svelte-preprocess";
 
 export default defineConfig({
-    server: {
-        port: 5000,
-    },
-    
-    plugins: [svelte()],
+  server: {
+    port: 5000,
+  },
+  plugins: [
+    svelte({
+      preprocess: [preprocess()],
+    }),
+  ],
 });
